1040ae138053e550b8bc7e4c2336061cba9184c5
Tom <tom.w.page@gmail.com>
2018-09-07 16:16:56 +0200

    Complete basic refactor

--- tennis_game1.rb
+++ tennis_game1.rb
@@ -1,62 +1,61 @@
 class TennisGame1
+  DRAW_SCOREBOARD = {
+    0 => 'Love-All',
+    1 => 'Fifteen-All',
+    2 => 'Thirty-All'
+  }.freeze
 
-  def initialize(player1Name, player2Name)
-    @player1Name = player1Name
-    @player2Name = player2Name
+  STND_SCOREBOARD = {
+    0 => 'Love',
+    1 => 'Fifteen',
+    2 => 'Thirty',
+    3 => 'Forty'
+  }.freeze
+
+  def initialize(player1_name, player2_name)
+    @player1_name = player1_name
+    @player2_name = player2_name
     @p1points = 0
     @p2points = 0
-    @stnd_score_board = {
-      0 => 'Love',
-      1 => 'Fifteen',
-      2 => 'Thirty',
-      3 => 'Forty'
-    }
   end
 
-  def won_point(playerName)
-    playerName == @player1Name ? @p1points += 1 : @p2points += 1
+  def won_point(player_name)
+    player_name == @player1_name ? @p1points += 1 : @p2points += 1
   end
 
   def score
     if @p1points == @p2points
       points_draw
     elsif @p1points >= 4 || @p2points >= 4
-      minus_result
+      win_advantage
     else
       normal_score
     end
   end
 
-  def normal_score
-    result = ['', '-', '']
-    (1...3).each do |i|
-      if i == 1
-        result[0] = @stnd_score_board[@p1points]
-      else
-        result[2] = @stnd_score_board[@p2points]
-      end
-    end
-    result.join('')
+  def points_draw
+    DRAW_SCOREBOARD.fetch(@p1points, 'Deuce')
   end
 
-  def points_draw
-    {
-      0 => 'Love-All',
-      1 => 'Fifteen-All',
-      2 => 'Thirty-All',
-    }.fetch(@p1points, 'Deuce')
+  def normal_score
+    [STND_SCOREBOARD[@p1points].to_s,
+     '-',
+     STND_SCOREBOARD[@p2points].to_s].join('')
   end
 
   def minus_result
-    minusResult = @p1points - @p2points
-    if minusResult == 1
-      'Advantage ' + @player1Name
-    elsif minusResult == -1
-      'Advantage ' + @player2Name
-    elsif minusResult >= 2
-      'Win for ' + @player1Name
+    @p1points - @p2points
+  end
+
+  def win_advantage
+    if minus_result == 1
+      'Advantage ' + @player1_name
+    elsif minus_result == -1
+      'Advantage ' + @player2_name
+    elsif minus_result >= 2
+      'Win for ' + @player1_name
     else
-      'Win for ' + @player2Name
+      'Win for ' + @player2_name
     end
   end
 end
