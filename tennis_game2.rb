455046dde5895d80a3cc696afb89e9b87760e1cc
Tom <tom.w.page@gmail.com>
2018-09-08 11:43:18 +0200

    Make first fund refactor

--- tennis_game2.rb
+++ tennis_game2.rb
@@ -1,4 +1,9 @@
 class TennisGame2
+  NORMAL_SCOREBOARD = %w[Love Fifteen Thirty Forty].freeze
+  DRAW_SCOREBOARD = %w[Love-All Fifteen-All Thirty-All].freeze
+  LOVE_SCOREBOARD1 = %w[placeholder Fifteen-Love Thirty-Love Forty-Love].freeze
+  LOVE_SCOREBOARD2 = %w[placeholder Love-Fifteen Love-Thirty Love-Forty].freeze
+
   def initialize(player1_name, player2_name)
     @player1_name = player1_name
     @player2_name = player2_name
@@ -12,107 +17,50 @@ class TennisGame2
   end
 
   def score
-    result = ''
     if @p1_points == @p2_points && @p1_points < 3
-      if @p1_points.zero?
-        result = 'Love'
-      end
-      if @p1_points == 1
-        result = 'Fifteen'
-      end
-      if @p1_points == 2
-        result = 'Thirty'
-      end
-      result += '-All'
-    end
-    if @p1_points == @p2_points && @p1_points > 2
+      result = draw_score
+    elsif @p1_points == @p2_points && @p1_points > 2
       result = 'Deuce'
     end
 
-    p1_res = ''
-    p2_res = ''
     if @p1_points > 0 && @p2_points.zero?
-      if @p1_points == 1
-        p1_res = 'Fifteen'
-      end
-      if @p1_points == 2
-        p1_res = 'Thirty'
-      end
-      if @p1_points == 3
-        p1_res = 'Forty'
-      end
-      p2_res = 'Love'
-      result = p1_res + '-' + p2_res
-    end
-    if @p2_points > 0 && @p1_points.zero?
-      if @p2_points == 1
-        p2_res = 'Fifteen'
-      end
-      if @p2_points == 2
-        p2_res = 'Thirty'
-      end
-      if @p2_points == 3
-        p2_res = 'Forty'
-      end
-
-      p1_res = 'Love'
-      result = p1_res + '-' + p2_res
+      result = p1_love_score(@p1_points)
+    elsif @p2_points > 0 && @p1_points.zero?
+      result = p2_love_score(@p2_points)
     end
 
     if @p1_points > @p2_points && @p1_points < 4
-      if @p1_points == 2
-        p1_res = 'Thirty'
-      end
-      if @p1_points == 3
-        p1_res = 'Forty'
-      end
-      if @p2_points == 1
-        p2_res = 'Fifteen'
-      end
-      if @p2_points == 2
-        p2_res = 'Thirty'
-      end
+      p2_res = NORMAL_SCOREBOARD[@p2_points]
+      p1_res = NORMAL_SCOREBOARD[@p1_points]
       result = p1_res + '-' + p2_res
     end
     if @p2_points > @p1_points && @p2_points < 4
-      if @p2_points == 2
-        p2_res = 'Thirty'
-      end
-      if @p2_points == 3
-        p2_res = 'Forty'
-      end
-      if @p1_points == 1
-        p1_res = 'Fifteen'
-      end
-      if @p1_points == 2
-        p1_res = 'Thirty'
-      end
+      p2_res = NORMAL_SCOREBOARD[@p2_points]
+      p1_res = NORMAL_SCOREBOARD[@p1_points]
       result = p1_res + '-' + p2_res
     end
     if @p1_points > @p2_points && @p2_points >= 3
       result = 'Advantage ' + @player1_name
-    end
-    if @p2_points > @p1_points && @p1_points >= 3
+    elsif @p2_points > @p1_points && @p1_points >= 3
       result = 'Advantage ' + @player2_name
     end
     if @p1_points >= 4 && @p2_points >= 0 && @p1_points - @p2_points >= 2
       result = 'Win for ' + @player1_name
-    end
-    if @p2_points >= 4 && @p1_points >= 0 && @p2_points - @p1_points >= 2
+    elsif @p2_points >= 4 && @p1_points >= 0 && @p2_points - @p1_points >= 2
       result = 'Win for ' + @player2_name
     end
     result
   end
 
-  def set_p1_score(number)
-    (0..number).each do |i|
-      p1_score
-    end
+  def draw_score
+    DRAW_SCOREBOARD[@p1_points]
   end
 
-  def set_p2_score(number)
-    (0..number).each do |i|
-      p2_score
-    end
+  def p1_love_score(p1_player)
+    LOVE_SCOREBOARD1[p1_player]
+  end
+
+  def p2_love_score(p1_player)
+    LOVE_SCOREBOARD2[p1_player]
   end
 end
